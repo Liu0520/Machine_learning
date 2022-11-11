ff298e37197e54e4a47ea02a8dc94c0fe9f22156
Liu0520 <76727042+Liu0520@users.noreply.github.com>
2022-11-11 19:27:22 +0800

    train.py

--- train.py
+++ train.py
@@ -1,37 +1,9 @@
 import numpy as np
-from torch import nn
 import lightgbm as lgb
+import pickle
 
 seq_len = 15
-input_size = 5
-hidden_size = 64
-num_layers = 3
-dropout = 0.5
-# per_iter = 100
 
-num_epochs = 500
-batch_size = 512
-lr = 1e-4
-
-
-class Model(nn.Module):
-    def __init__(self, input_size=5, hidden_size=64, output_size=5, num_layers=3, dropout=0.5):
-        super(Model, self).__init__()
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
-        self.dropout = nn.Dropout(p=dropout)
-        self.fc = nn.Linear(in_features=hidden_size, out_features=output_size)
-
-    def forward(self, x):
-        x, _ = self.lstm(x)
-        x = x[:, -1]
-        self.dropout(x)
-        out = self.fc(x)
-        return out
-
-
-# 读取已经保存的特征
-# features=['event_type','product_id' , 'category_id', 'brand', 'price']
-# labels=['product_id']
 train_features, train_labels = np.load('train_features.npy'), np.load('train_labels.npy')
 test_features, test_labels = np.load('test_features.npy'), np.load('test_labels.npy')
 # 竖直方向进行了堆叠
@@ -41,32 +13,35 @@ labels = np.hstack((train_labels, test_labels))
 
 # 将标签进行转换
 features_, labels_ = [], []
-# features(230552, 25, 5)   labels(230553,)
+# features(181027, 15, 5)   labels(181027,)
 for f, l in zip(features, labels):
+    # 如果商品ID为876，且在前面1-15个数据里第一个商品ID也为876的话，那么返回值为0，
+    # 如果这一组数据里都没有该商品ID就返回15
+    # 查找商品ID,并返回一个0-15的值
     idx = np.where(f[:, 1] == l)[0]
-    # print(f[:,1])
-    # 以元组的形式返回
     if len(idx) == 0:
         labels_.append(seq_len)
     else:
-        # np.random.choice()从数组中随机抽取元素
         labels_.append(np.random.choice(idx))
 labels = np.array(labels_)
+class_weight = {}
+for i in np.unique(labels):
+    if i != seq_len:
+        class_weight[i] = 1.
+    else:
+        class_weight[i] = 1 / 3
 print(labels)
 print(features.shape, labels.shape)
 
-# 样本平衡
-num_samples = len(features)
 from collections import Counter
 
 print(Counter(labels))
-print(features.shape, Counter(labels))
-# features = features.reshape([len(features), seq_len, 5])
+print(features.shape)
 
-# 将列变成行
+
+# 三维变二维，合并后面的5x15,变为(181027,75)
 features = features.reshape([len(features), -1])
-import pickle
-
+print(features.shape)
 
 def pkl_save(filename, file):
     output = open(filename, 'wb')
@@ -84,15 +59,17 @@ def pkl_load(filename):
 if __name__ == '__main__':
     model = lgb.LGBMClassifier(
         boosting_type='gbdt',
+        num_leaves=32,
         max_depth=100,
-        n_estimators=1000,
+        learning_rate=0.05,
+        n_estimators=1500,
+        class_weight=class_weight,
     )
-
     print('Training ...')
-    model.fit(features, labels, eval_set=[(features, labels)], verbose=True, )
-
+    model.fit(features, labels,)
+    # 多分类的对数损失
     y_pred = model.predict(features)
+    # 平均精度得分
     acc = np.mean(y_pred == labels)
-    print(acc)
-
+    print(acc)#0.9023
     pkl_save('model.pkl', model)
